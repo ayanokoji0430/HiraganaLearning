90a4329ef8da86dac1e85da023b8e6463109a081
ayanokoji0430 <bekh1240036@gn.iwasaki.ac.jp>
2016-06-20 14:58:17 +0900

    結果をテキストファイルに書き込むクラス

---
+++ 養護学校アプリ/ResultWrite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace 養護学校アプリ
+{
+    class ResultWrite
+    {
+        public void ResultWriter(Hashtable Wrong_Words,string[] Question_Words)
+        {
+            StreamWriter writer =new StreamWriter(@"result.txt",false);
+            foreach (string word in Question_Words)
+            {
+                writer.WriteLine(word);
+                writer.WriteLine((string)Wrong_Words[word]);
+            }
+            writer.Close();
+        }
+    }
+}
